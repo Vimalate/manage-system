d6a407313de1af4790b5a517873c95222d7ac401
“Vimalate <1902644369@qq.com>
2020-06-18 23:51:17 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Vimalakirti
+ * @Date: 2020-06-18 23:37:58
+ * @LastEditTime: 2020-06-18 23:38:58
+ * @Description: 
+ * @FilePath: \vue项目\vue-manage-system\vue.config.js
+ */ 
+module.exports={
+  devServer:{
+    port:5555,
+    open:true
+  }
+}
