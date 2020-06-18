fce78e87aa0930d769084158d0468a82d1cb5f9e
“Vimalate <1902644369@qq.com>
2020-06-19 00:29:00 +0800

    配置scss

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <!--
  * @Author: Vimalakirti
  * @Date: 2020-06-18 23:34:42
- * @LastEditTime: 2020-06-19 00:08:00
+ * @LastEditTime: 2020-06-19 00:22:03
  * @Description: 
  * @FilePath: \vue-manage-system\src\App.vue
 -->
 <template>
   <div id="app">
+    app
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: $theme-color;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,21 @@
 /*
  * @Author: Vimalakirti
  * @Date: 2020-06-18 23:37:58
- * @LastEditTime: 2020-06-18 23:38:58
- * @Description: 
- * @FilePath: \vue项目\vue-manage-system\vue.config.js
- */ 
-module.exports={
-  devServer:{
-    port:5555,
-    open:true
+ * @LastEditTime: 2020-06-19 00:28:07
+ * @Description:
+ * @FilePath: \vue-manage-system\vue.config.js
+ */
+
+module.exports = {
+  devServer: {
+    port: 5555,
+    open: true
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/assets/scss/_variable.scss";`
+      }
+    }
   }
-}
+};
